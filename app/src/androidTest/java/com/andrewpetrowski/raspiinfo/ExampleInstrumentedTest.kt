4b049541bb41df41402d77e0faf37055f043763b
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-30 12:55:35 +0200

    Tests

--- app/src/androidTest/java/com/andrewpetrowski/raspiinfo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/andrewpetrowski/raspiinfo/ExampleInstrumentedTest.kt
@@ -2,6 +2,9 @@ package com.andrewpetrowski.raspiinfo
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import com.andrewpetrowski.diploma.bridgelib.Controllers.DhtController
+import com.andrewpetrowski.diploma.bridgelib.Models.DHT11_Data
+import org.junit.Assert
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,5 +23,17 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.andrewpetrowski.raspiinfo", appContext.packageName)
+    }
+
+    @Test
+    fun getLast() {
+        try {
+            var dhtController: DhtController = DhtController()
+            var dtData:DHT11_Data = dhtController.GetLast(DHT11_Data::class.java)
+            assertEquals(dtData.temperature,25f,1e-5f)
+        } catch (ex: Exception) {
+            System.out.println(ex.message)
+            assertFalse(true)
+        }
     }
 }
