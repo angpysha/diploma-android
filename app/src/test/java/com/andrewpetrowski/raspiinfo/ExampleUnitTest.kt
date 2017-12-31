530da34a93da9526ef9eec31892ef75fb28770b1
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-31 12:58:09 +0200

    Unit testing

--- app/src/test/java/com/andrewpetrowski/raspiinfo/ExampleUnitTest.kt
+++ app/src/test/java/com/andrewpetrowski/raspiinfo/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.andrewpetrowski.raspiinfo
 
+import com.andrewpetrowski.raspiinfo.Controllers.TemperatureController
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +17,22 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testDateTemperature() {
+        try {
+            val temperatureContorller = TemperatureController()
+
+            val date: Date = Date.from(LocalDate.of(2017, 12, 29).atStartOfDay(ZoneId.systemDefault())
+                    .toInstant())
+            var data = temperatureContorller.GetByDate(date)
+
+            assertEquals(data!![0].temperature,25f,1e-3f)
+
+        } catch (ex: Exception)
+        {
+            ex.printStackTrace()
+        }
     }
 }
