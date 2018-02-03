1a48f75dda70febc3ec52291c7c884188f48d115
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-02-04 00:05:41 +0200

    Changes for type switching for Date-Page number converter

--- app/src/main/java/com/andrewpetrowski/raspiinfo/Models/PageDatePair.kt
+++ app/src/main/java/com/andrewpetrowski/raspiinfo/Models/PageDatePair.kt
@@ -28,42 +28,53 @@ import kotlin.collections.LinkedHashMap
  */
 
 class PageDatePair(val size: Int) {
-    private lateinit var data : MutableMap<Int,Date>
+    private lateinit var data: MutableMap<Int, Date>
+
     init {
-        data = LinkedHashMap<Int,Date>()
-        var incr = size-1
+        data = LinkedHashMap<Int, Date>()
+        var incr = size - 1
         var date = Date()
         while (incr >= 0) {
             if (checkAvailable(date)) {
-            data.put(incr,date)
-            incr--
-            date = date.Decrement()
+                data.put(incr, date)
+                incr--
+                date = date.Decrement()
             } else {
                 date = date.Decrement()
             }
         }
     }
 
-    fun Get(page: Int) : Date? {
+    fun Get(page: Int): Date? {
         val entr = data.get(page)
 
         return entr
     }
 
     companion object {
-        fun GetDate(pos: Int,size: Int) : Date {
-            var minus = size-pos
+        fun GetDate(pos: Int, size: Int, type: Int): Date {
+            var minus = size - pos - 1
             val adh = AndroidDHTController()
             var cal = Calendar.getInstance()
+            cal.time = Date()
 
-            cal.add(Calendar.DATE,-minus)
-
-//            while (minus >=0) {
-//                 cal.add(Calendar.DATE,-1)
-//                if (adh.GetByDate(cal.time.zeroTime()).isNotEmpty())
-//                minus--
-//            }
+            when (type) {
+                0 -> cal.add(Calendar.DATE, -minus)
+                1 -> cal.add(Calendar.WEEK_OF_YEAR,-minus)
+                2->cal.add(Calendar.MONTH,-minus)
+                3->cal.add(Calendar.YEAR,-minus)
+            }
             return cal.time.zeroTime()
+        }
+
+        fun GetDate(pos: Int, size: Int): Date {
+            return GetDate(pos,size,0)
+        }
+
+        fun GetByDate(date: Date, size: Int): Int {
+            var diff = size - ((Date().zeroTime().time - date.zeroTime().time) / (1000 * 60 * 60 * 24)).toInt() - 1
+
+            return diff
         }
     }
 }
